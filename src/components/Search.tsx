47d55dab6fd9758944b4fe446e690e204e486dd8
Mahmoud Hasanzadeh <mahmoodh1371@gmail.com>
2024-02-14 10:50:31 +0330

    add filter components, responsive all components

---
+++ src/components/Search.tsx
@@ -0,0 +1,8 @@
+import { Input } from "antd";
+import "assets/styles/components/Search.css";
+
+const Search = () => (
+  <Input className="search" placeholder="Search any fields of the columns" />
+);
+
+export default Search;
